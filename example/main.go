79b227b578d5e85e47cd48ca11f62d65a0f1f6fb
alex cai <caiyingyao@ibbd.net>
2016-07-01 15:57:27 +0800

    example

--- example/main.go
+++ example/main.go
@@ -5,15 +5,15 @@ import "github.com/cyy0523xc/go-17monip"
 func main() {
 	ip.Load("../17monipdb.dat")
 
-	address := ip.Find("8.8.8.8") //address: GOOGLE\tGOOGLE\t
+	address := ip.Find("8.8.8.8") // output: GOOGLE\tGOOGLE\t
 	println(address)
 
-	address = ip.Find("202.106.46.151") //address: 中国\t北京\t
+	address = ip.Find("202.106.46.151") // output: 中国\t北京\t北京
 	println(address)
 
-	address = ip.Find("202.115.128.64") //address: 中国\t四川\t成都理工大学
+	address = ip.Find("202.115.128.64") // output: 中国\t四川\t成都
 	println(address)
 
-	address = ip.Find("116.22.198.67") //address: 中国\t四川\t成都理工大学
+	address = ip.Find("116.22.198.67") // output: 中国\t广东\t广州
 	println(address)
 }
